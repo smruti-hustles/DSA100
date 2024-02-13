dfcfa66c3a575167c33daf14ae3413cf8dc20d0a
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-02-13 21:44:18 +0530

    Update P53Automorphic.py

--- DAY-6 allbasics/P53Automorphic.py
+++ DAY-6 allbasics/P53Automorphic.py
@@ -11,3 +11,20 @@ if s3==s:
     print("yes")
 else:
     print("No")
+
+
+'''or
+n=int(input())
+sq=n**2
+n1=str(n)
+l1=len(n1)
+s1=str(sq)
+l2=len(s1)
+s2=s1[l2-l1:l2]
+print(n1)
+print(s2)
+if(n1==s2):
+    print("Automorphic numbers")
+else:
+    print("Not automorphic numbers")
+'''
