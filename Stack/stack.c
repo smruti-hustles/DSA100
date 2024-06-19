9c4e37eaf56db7fd197a2315dadff341a9995657
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-06-19 23:15:08 +0530

    Add files via upload

---
+++ Stack/stack.c
@@ -0,0 +1,77 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int top=-1;
+int stack[8];
+int MAXSIZE=8;
+
+//let's check if stack is empty
+int isempty(){
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
+//let's check if stack is full
+int isfull(){
+    if(top==MAXSIZE)
+        return 1;
+    else
+        return 0;
+}
+
+//fun to push an eleemnt using push()
+int push(int data){
+    if(!isfull()){
+        top=top+1;
+        stack[top]=data;
+    }
+    else{
+        printf("Stack is full, can't push anything");
+    }
+}
+
+//fun to pop an element using pop()
+int pop(){
+    int data;
+    if(!isempty()){
+        data=stack[top];
+        top=top-1;
+        return data;
+    }
+    else{
+        printf("Stack is empty, can't delete anything");
+    }
+}
+
+
+//function to retrieve the top element using peek()
+int peek(){
+    return stack[top];
+}
+
+
+
+int main()
+{
+    push(350);
+    push(135);
+    push(305);
+    push(1235);
+    push(35);
+    printf("Element at the top : %d \n",peek());
+    
+    printf("The elements are \n");
+    while(!isempty()){
+        int data=pop();
+        printf("%d ", data);
+    }
+
+    return 0;
+}
