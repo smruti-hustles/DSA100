7a3a3c41e1a6cc01de06a45d612220314480c075
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-06-19 23:29:32 +0530

    Update stack.c

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,8 +1,6 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+                           STACK IMPLEMENTATION
 
 *******************************************************************************/
 
